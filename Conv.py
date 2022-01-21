b0ce82406a61f056748fa131282e9640ad55edfc
madlinq <markoff.u.s@gmail.com>
2022-01-21 23:21:13 +0300

    Some Changes

---
+++ Conv.py
@@ -0,0 +1,18 @@
+class Conv():
+
+    def __init__(self, degree):
+        self.degree = degree
+
+    def fahrenheit(self):
+        result = (self.degree - 32) * 5 / 9
+        if result < -273.15:
+            return('Temperature below absolute zero')
+        else:
+            return(self.degree, ' degrees Fahrenheit equals ', round(result, 1), ' degrees Celcius')
+
+    def celcius(self):
+        result = (self.degree * 9 / 5) + 32
+        if result < -459.67:
+            return('Temperature below absolute zero')
+        else:
+            return self.degree, ' degrees Celcius equals ', round(result, 1), ' degrees Fahrenheit'
--- far2cel.py
+++ far2cel.py
@@ -1,36 +1,24 @@
 import re
+from Conv import Conv
 
 
 def main():
-    data = input(r'Enter the value to convert(e.g. 40f or 40c): ')
-    try:
-        regex = re.search(r'([-+]?\d+)([c,C]$|[f,F]$)', data)
-        result, degree, prefix = str(regex[0]), int(regex[1]), str(regex[2].lower())
-    except(TypeError, AttributeError):
-        regex = ''
+    data = input(
+        'Enter the value to convert(e.g. 40f or 40c): ').lower()
+    if '+' in data:
+        data = data[1:]
+    regex = re.search(r'(^\-?\d+[\.|,]?\d*)([c|f]{1}$)', data)
+    if regex != None:
+        degree, prefix = float(regex[1]), str(regex[2])
+        t = Conv(degree)
+        if 'f' in prefix:
+            print(''.join(str(i) for i in t.fahrenheit()))
+        else:
+            print(''.join(str(i) for i in t.celcius()))
+        return main()
+    else:
         print('Something is wrong!')
         return main()
-
-    while re.match(result, data):
-        if 'f' in prefix:
-            fahrenheit(degree)
-        else:
-            celcius(degree)
-    else:
-        print('Error')
-        return main()
-
-
-def fahrenheit(degree):
-    far = (degree - 32) * 5 / 9
-    print(degree, 'degrees Fahrenheit equals ', far, 'degrees Celcius')
-    return main()
-
-
-def celcius(degree):
-    cel = (degree * 9 / 5) + 32
-    print(degree, 'degrees Celcius equals ', cel, 'degrees Fahrenheit')
-    return main()
 
 
 if __name__ == '__main__':
