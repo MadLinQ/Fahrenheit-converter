915069361c4cf69aa41aac1ffa21ea0602946c5f
madlinq <markoff.u.s@gmail.com>
2022-01-22 16:51:05 +0300

    Some Changes

--- far2cel.py
+++ far2cel.py
@@ -4,8 +4,7 @@ from Conv import Conv
 
 def main():
     data = input(
-        'Enter the value to convert(e.g. 40f or 40c)\
-        \nor type exit to close programm: ').lower()
+        'Enter the value to convert(e.g. 40f or 40c): ').lower()
     regex = re.search(r'(^\-?\d+\.?\d*)(\s?)([c|f]{1}$)', data)
     if regex != None:
         degree, prefix = float(regex[1]), str(regex[3])
