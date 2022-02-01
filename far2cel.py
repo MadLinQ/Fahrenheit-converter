7355543b9f0fc0a63c226d9faf6155dbd49d0e93
madlinq <markoff.u.s@gmail.com>
2022-02-01 21:08:53 +0300

    Some Changes

--- far2cel.py
+++ far2cel.py
@@ -5,10 +5,10 @@ from Conv import Conv
 def main():
     data = input(
         'Enter the value to convert(e.g. 40f or 40c): ').lower()
-    regex = re.search(r'(^-?\d+\.?\d*)(\s?)([c|f]{1}$)', data)
+    regex = re.search(r'(^-?\d+\.?\d*)(\s?)([c|f]$)', data)
     if regex is not None:
-        degree, prefix = float(regex[1]), str(regex[3])
-        if 'f' in prefix:
+        degree, key = float(regex[1]), str(regex[3])
+        if 'f' in key:
             print(''.join(str(i) for i in Conv(degree).fahrenheit()))
         else:
             print(''.join(str(i) for i in Conv(degree).celsius()))
