7d311c86e20282cecabdff62a5ac487b076b2069
Yuri Markoff <1761324+MadLinQ@users.noreply.github.com>
2022-02-05 13:43:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from Conv import Conv
 
 
 def main():
-    data = input(
-        'Enter the value to convert(e.g. 40f or 40c): ').lower()
-    regex = re.search(r'(^-?\d+\.?\d*)(\s?)([c|f]$)', data)
-    if regex is not None:
-        degree, key = float(regex[1]), str(regex[3])
-        if 'f' in key:
-            print(''.join(str(i) for i in Conv(degree).fahrenheit()))
+    while True:
+        data = input(
+            'Enter the value to convert(e.g. 40f or 40c): ').lower()
+        regex = re.search(r'(^-?\d+\.?\d*)(\s?)([c|f]$)', data)
+        if regex is not None:
+            degree, key = float(regex[1]), str(regex[3])
+            if 'f' in key:
+                print(''.join(str(i) for i in Conv(degree).fahrenheit()))
+            else:
+                print(''.join(str(i) for i in Conv(degree).celsius()))
         else:
-            print(''.join(str(i) for i in Conv(degree).celsius()))
-    else:
-        print('Something is wrong!')
+            print('Something is wrong!')
+            break
 
 
 if __name__ == '__main__':
