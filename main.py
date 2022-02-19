9847f2f7d5b7d074088a82c5f2d512c216e56c3a
madlinq <markoff.u.s@gmail.com>
2022-02-19 09:16:03 +0300

    Some Changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import re
-from Conv import Conv
 
 
 def main():
@@ -10,9 +9,11 @@ def main():
         if regex is not None:
             degree, key = float(regex[1]), str(regex[3])
             if 'f' in key:
-                print(''.join(str(i) for i in Conv(degree).fahrenheit()))
+                print(''.join(str(i) for i in (degree, ' degrees Fahrenheit equals ',
+                    round((lambda x: (x - 32) * 5 / 9)(degree), 1), ' degrees Celsius')))
             else:
-                print(''.join(str(i) for i in Conv(degree).celsius()))
+                print(''.join(str(i) for i in (degree, ' degrees Celsius equals ',
+                    round((lambda x: (x * 9 / 5) + 32)(degree), 1), ' degrees Fahrenheit')))
         else:
             print('Something is wrong!')
             break
